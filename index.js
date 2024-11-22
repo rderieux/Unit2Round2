e058721fbafa05f66280c8a070e6535cc6ca1696
Robert DeRieux <ppldiecomplex@gmail.com>
2024-11-22 13:44:33 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let official = prompt(
+  "What's the official name of JavaScript",
+  "Answer please?"
+);
+
+if (official === "ECMAScript") {
+  alert("Correct!");
+} else {
+  alert("You don't know?  It's ECMAScript!");
+}
